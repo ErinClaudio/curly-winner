5fd3bd772d6a54f791858b474b562ef1cf742365
Erin Claudio <eclaudio@gmail.com>
2020-07-14 14:19:34 -0400

    item.py endpoints methods changed for a SQLite3 db and not a inline db

--- code/app.py
+++ code/app.py
@@ -1,38 +1,19 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_jwt import JWT, jwt_required
-
-from code.security import authenticate, identity
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from item import Item, ItemList
+from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = 'erin1234'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
-items = []
-
-
-class Item(Resource):
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)  # Clearer as a list comprehension
-        return {"item": item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': 'an item with name {} already exists '. format(name)}, 400
-        data = request.get_json()
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
-
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
